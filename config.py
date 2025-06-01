82549e3c75aa4532067ef56ddc3788950541e7fb
Harsh-Karia <64611524+Harsh-Karia@users.noreply.github.com>
2025-05-31 20:09:03 -0700

    Re-add sanitized config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+# Usually would not push or share, but provided here as a reference in case my api_key and/or my database needs to be used.
+
+API_KEY = "NOTION API KEY HERE"
+DATABASE_ID = "NOTION DATABASE ID HERE"
